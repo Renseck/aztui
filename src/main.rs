cc9df0299f9f76bddc54293c199fefef23fa2c20
Renseck <rensvaneck@gmail.com>
2026-04-11 08:00:31 +0200

    Cleanup module declaration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-mod app;
-mod cache;
-mod command;
-mod config;
-mod domain;
-mod errors;
-mod event;
-mod providers;
-mod security;
-mod ui;
-
-fn main() {}
+fn main() {
+    // use aztui::...
+}
--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -2,4 +2,4 @@ pub mod auth_provider;
 pub mod cost_provider;
 pub mod resource_provider;
 
-pub use auth_provider::AzAuthProvider;
+pub use auth_provider::AzAuthProvider; 
